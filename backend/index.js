830bd97f3f20a4ec6c1ffcd824f0e87409ecda15
Hassam-Ata <hassamata86@gmail.com>
2025-01-02 03:04:08 +0500

    added origin in cors

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,11 @@ const PORT = process.env.port || 5000;
 
 // Configure CORS
 app.use(cors());
+app.use(
+  cors({
+    origin: "https://articulate-client.vercel.app",
+  })
+);
 
 app.use(clerkMiddleware());
 app.use(express.json());
