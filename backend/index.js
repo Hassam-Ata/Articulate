29b857b1f9046978a0bad7e6a7ab75af9b2f4a6b
Hassam-Ata <hassamata86@gmail.com>
2025-01-02 03:55:34 +0500

    kuch changes

--- backend/index.js
+++ backend/index.js
@@ -8,22 +8,20 @@ import { clerkMiddleware, requireAuth } from "@clerk/express";
 import cors from "cors";
 
 const app = express();
+
+// Manually set CORS headers
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
     "https://articulate-client.vercel.app"
-  ); // Allow only your frontend
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Allow specific HTTP methods
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow specific headers
-  res.setHeader("Access-Control-Allow-Credentials", true); // If you need to send cookies or authentication headers
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
+
 const PORT = process.env.port || 5000;
-// const CLIENT_URL =
-//   process.env.CLIENT_URL || "https://articulate-client.vercel.app";
-
-app.use(cors());
-
 
 app.use(clerkMiddleware());
 app.use(express.json());
