385eab3afb8791ae050e07d97aca5819f8722c5b
Hassam-Ata <hassamata86@gmail.com>
2025-01-03 00:07:10 +0500

    some changes in connectDB

--- backend/lib/connectDB.js
+++ backend/lib/connectDB.js
@@ -1,10 +1,19 @@
 import mongoose from "mongoose";
 
+let cachedConnection = null;
+
 const connectDB = async () => {
+  if (cachedConnection) return cachedConnection;
+
   try {
     const uri = process.env.MONGO;
-    await mongoose.connect(uri);
-    console.log("MongoDB is connected");
+    const connection = await mongoose.connect(uri, {
+      serverSelectionTimeoutMS: 5000,
+      connectTimeoutMS: 10000
+    });
+    cachedConnection = connection;
+    console.log("MongoDB connected");
+    return connection;
   } catch (err) {
     console.error("MongoDB connection error:", err);
     throw err;
