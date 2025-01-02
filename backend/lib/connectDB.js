5048e70d5fd2d96cd9424dce54e63f4396bb3156
Hassam-Ata <hassamata86@gmail.com>
2025-01-03 00:30:44 +0500

    removed vercel

--- backend/lib/connectDB.js
+++ backend/lib/connectDB.js
@@ -1,19 +1,10 @@
 import mongoose from "mongoose";
 
-let cachedConnection = null;
-
 const connectDB = async () => {
-  if (cachedConnection) return cachedConnection;
-
   try {
     const uri = process.env.MONGO;
-    const connection = await mongoose.connect(uri, {
-      serverSelectionTimeoutMS: 5000,
-      connectTimeoutMS: 10000
-    });
-    cachedConnection = connection;
-    console.log("MongoDB connected");
-    return connection;
+    await mongoose.connect(uri);
+    console.log("MongoDB is connected");
   } catch (err) {
     console.error("MongoDB connection error:", err);
     throw err;
