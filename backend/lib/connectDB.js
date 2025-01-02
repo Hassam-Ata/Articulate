0947901c9e425af7b2ca3c5d8ba321a6d5f35a59
Hassam-Ata <hassamata86@gmail.com>
2025-01-02 23:04:37 +0500

    added json

--- backend/lib/connectDB.js
+++ backend/lib/connectDB.js
@@ -1,15 +1,13 @@
 import mongoose from "mongoose";
 
-// process.env.MONGO || 
-
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGODB_URI; // Try Vercel URI first, fallback to local
+    const uri = process.env.MONGO;
     await mongoose.connect(uri);
     console.log("MongoDB is connected");
   } catch (err) {
     console.error("MongoDB connection error:", err);
-    throw err; // Re-throw to handle in parent
+    throw err;
   }
 };
 
